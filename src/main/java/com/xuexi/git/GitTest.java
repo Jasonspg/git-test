52bdff2b7f5a9bdc4e28db271d0a9634bf7fd5ec
shupingguo <m18222111816@163.com>
2022-04-30 16:49:27 +0800

    second commit

--- src/main/java/com/xuexi/git/GitTest.java
+++ src/main/java/com/xuexi/git/GitTest.java
@@ -4,5 +4,6 @@ package com.xuexi.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello github");
     }
 }
